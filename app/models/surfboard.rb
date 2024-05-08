9da3cf21a4d590f16c20b71ba0806cd87b9cc0af
Muhammad Rizky <rizky.wazza@gmail.com>
2024-05-08 20:11:46 +0200

    update done

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -5,4 +5,5 @@ class Surfboard < ApplicationRecord
   enum :status, { pending: 0, approve: 1, decline: 2 }
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  validates :price, numericality: { only_integer: true, greater_than: 0 }
 end
